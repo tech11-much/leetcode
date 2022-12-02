bb8508e6a7b85a47c42a325f93e323af09cc4cb9
tech11-much <18348792873@163.com>
2022-12-02 22:31:41 +0800

    new

---
+++ src/0005_test.go
@@ -0,0 +1,30 @@
+package src
+
+import "testing"
+
+func Test_longestPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			"test",
+			args{
+				"ddddddd",
+			},
+			"djjjdjdjdj",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
